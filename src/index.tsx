cf4d4503a0340950be60d9f76494505ca5871a44
Nathaniel M. Capule <nmcapule@gmail.com>
2017-10-25 04:39:13 +0800

    Reactiv buttons and label
    |

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import App from './App';
-import Hello from './components/Hello';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+
+import Hello from './containers/Hello';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
+
 ReactDOM.render(
-  <Hello name="Typescript" enthusiasmLevel={5} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
